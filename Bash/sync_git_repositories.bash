989ac3b51e1f2f7f92e6a846c05312bea87bce5a
XJDHDR <XJDHDR@users.noreply.github.com>
2019-06-25 02:21:09 +1200

    Automatic update of Adblock, Bash + blocklist files

--- Bash/sync_git_repositories.bash
+++ Bash/sync_git_repositories.bash
@@ -14,33 +14,31 @@ then
 fi
 
 {
-	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' fetch --prune-tags >'/dev/null'
-	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' diff master origin/master --name-only >'/home/git_user/github_wiki_changes.txt'
-	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' pull --no-edit --no-log --strategy-option=theirs >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' fetch --prune-tags github-fetch master >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' diff --name-only master github-fetch/master >'/home/git_user/github_wiki_changes.txt'
 
-	git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' fetch --prune-tags >'/dev/null'
-	git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' diff master origin/master --name-only >'/home/git_user/gitlab_wiki_changes.txt'
-	git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' pull --no-edit --no-log --strategy-option=theirs >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' fetch --prune-tags bitbucket-fetch master >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' diff --name-only master bitbucket-fetch/master >'/home/git_user/bitbucket_wiki_changes.txt'
 
-	unison -silent -batch -times -ignore "Name .git" '/home/git/github_wiki' '/home/git/gitlab_wiki'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' fetch --prune-tags gitlab-fetch master >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' diff --name-only master gitlab-fetch/master >'/home/git_user/gitlab_wiki_changes.txt'
 
-	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' add --all
-	git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' add --all
-	if ! git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' diff-index --quiet HEAD --
-	then
-		git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' commit --all --message='Automated sync with GitLab wiki' --quiet
-		sshpass -f "$HOME/github_password.txt" -P "nter passphrase for key" git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' push origin/master master
-	fi
-	if ! git -C '/home/git/github_wiki/' --git-dir='/home/git/gitlab_wiki/.git' diff-index --quiet HEAD --
-	then
-		git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' commit --all --message='Automated sync with GitHub wiki' --quiet
-		sshpass -f "$HOME/gitlab_password.txt" -P "nter passphrase for key" git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' push origin/master master
-	fi
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' merge --no-edit --no-log master github-fetch/master >'/dev/null'
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' merge --no-edit --no-log master bitbucket-fetch/master >'/dev/null'
+	git -C '/home/git/gitlab_wiki/' --git-dir='/home/git/gitlab_wiki/.git' merge --no-edit --no-log master gitlab-fetch/master >'/dev/null'
+
+#	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' add --all
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' push github master
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' push bitbucket master
+	git -C '/home/git/github_wiki/' --git-dir='/home/git/github_wiki/.git' push gitlab master
 
 	sFilesEdited_GitHub_wiki=$(cat '/home/git_user/github_wiki_changes.txt')
+	sFilesEdited_Bitbucket_wiki=$(cat '/home/git_user/bitbucket_wiki_changes.txt')
 	sFilesEdited_GitLab_wiki=$(cat '/home/git_user/gitlab_wiki_changes.txt')
 
+	grep --fixed-strings --line-regexp -f '/home/git_user/github_wiki_changes.txt' '/home/git_user/bitbucket_wiki_changes.txt' >'/home/git_user/matching_lines.txt'
 	grep --fixed-strings --line-regexp -f '/home/git_user/github_wiki_changes.txt' '/home/git_user/gitlab_wiki_changes.txt' >'/home/git_user/matching_lines.txt'
+	grep --fixed-strings --line-regexp -f '/home/git_user/bitbucket_wiki_changes.txt' '/home/git_user/gitlab_wiki_changes.txt' >'/home/git_user/matching_lines.txt'
 	if [ -f '/home/git_user/matching_lines.txt' ]
 	then
 		sMergeConflicts+=$(cat '/home/git_user/matching_lines.txt')
@@ -48,6 +46,7 @@ fi
 	fi
 
 	rm -f '/home/git_user/github_wiki_changes.txt'
+	rm -f '/home/git_user/bitbucket_wiki_changes.txt'
 	rm -f '/home/git_user/gitlab_wiki_changes.txt'
 } 2> '/tmp/stderr-contents-sync_git_repositories.txt'
 
@@ -61,10 +60,7 @@ if [ "$(du -bs /home/git/github_wiki | cut -f1)" -gt 2147483648 ]	# If more than
 then
 	sErrors+=$(printf '/home/git/github_wiki is more than 2GB in size')
 fi
-if [ "$(du -bs /home/git/gitlab_wiki | cut -f1)" -gt 2147483648 ]	# If more than 2GB
-then
-	sErrors+=$(printf '/home/git/gitlab_wiki is more than 2GB in size')
-fi
+
 
 if [[ -n "$sErrors" ]] || [[ -n "$sMergeConflicts" ]] || [[ -n "$sFilesEdited_GitHub_wiki" ]] || [[ -n "$sFilesEdited_GitLab_wiki" ]]
 then
@@ -79,12 +75,17 @@ fi
 
 if [[ -n "$sMergeConflicts" ]]
 then
-	printf 'The following merge conflicts were found:\n%s\n\n' "$sMergeConflicts" >> '/home/error_reports_to_email.txt'
+	printf 'The following files were edited in two or more wikis simultaneously. Check for merge conflicts:\n%s\n\n' "$sMergeConflicts" >> '/home/error_reports_to_email.txt'
 fi
 
 if [[ -n "$sFilesEdited_GitHub_wiki" ]]
 then
 	printf 'The following pages were edited on the GitHub wiki:\n%s\n\n' "$sFilesEdited_GitHub_wiki" >> '/home/error_reports_to_email.txt'
+fi
+
+if [[ -n "$sFilesEdited_Bitbucket_wiki" ]]
+then
+	printf 'The following pages were edited on the Bitbucket wiki:\n%s\n\n' "$sFilesEdited_Bitbucket_wiki" >> '/home/error_reports_to_email.txt'
 fi
 
 if [[ -n "$sFilesEdited_GitLab_wiki" ]]
